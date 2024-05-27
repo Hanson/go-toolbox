524f543d759f75d9fa3c0cce58b0baba97554f42
Hanson <h@hanc.cc>
2024-05-27 14:12:33 +0800

    add StartOfDayWithTime

--- carbon/timestamp.go
+++ carbon/timestamp.go
@@ -7,3 +7,8 @@ func StartOfDay() time.Time {
 	loc, _ := time.LoadLocation("Local")
 	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, loc)
 }
+
+func StartOfDayWithTime(t time.Time) time.Time {
+	loc, _ := time.LoadLocation("Local")
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, loc)
+}
